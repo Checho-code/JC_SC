cb2ae23076601459553e89a198a44f3cc25ab3a4
Checho-code <codigopractico23@gmail.com>
2023-03-04 11:07:33 -0500

    Se corrigen errores en modal comprar y su css

--- vistas/mod/mod_comprar_prod.php
+++ vistas/mod/mod_comprar_prod.php
@@ -1,7 +1,7 @@
 <!-- este modal es para usuarios logueados  (data-backdrop="static" data-keyboard="false"  esto evita que modalse cierre)-->
 
 <!--ventana para Update--->
-<div class="modal fade" id="verProdConLogueo<?php echo $consulta['id_producto'] ?>" tabindex="-1" role="dialog"
+<div class="modal fade" id="verProdConLogueo<?php echo $consulta['id_producto']; ?>" tabindex="-1" role="dialog"
     aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog  modal-lg">
         <div class="modal-content">
@@ -21,27 +21,29 @@
             <form method="POST" action="../index-base.php">
                 <input type="hidden" name="id" value="<?php echo $consulta['id_producto']; ?>">
                 
-                <input class="nompro" name="nom_producto" readonly value="<?php echo $consulta['nom_producto'] ?>">
+                <input class="nompro" name="nom_producto" readonly value="<?php echo $consulta['nom_producto']; ?>">
 
                 <div class="row justify-content-around mb-5  p-2">
 
                     <div class="contimg col-5 ">
+                    <input type="hidden" name="img" value=" <?php echo $consulta['imagen'] ?>"> 
                         <img class="images" src="../images/img_productos/<?php echo $consulta['imagen'] ?>"
-                            alt="Imagen  <?php echo $consulta['nom_producto'] ?>">
+                            alt="Imagen  <?php echo $consulta['nom_producto']; ?>">
                     </div>
 
                     <div class="context col-5 ">
                         <label for="descripcion" class="label">Descripcion: </label>
                         <div class="ccont col-12">
-                        <p class="entra" name="descripcion"><?php echo $consulta['descripcion'] ?> </p>  
+                        <input type="hidden" name="descripcion" value=" <?php echo $consulta['descripcion'] ?>"> 
+                        <p class="entra"> <?php echo $consulta['descripcion'] ?></p>
                         </div>
                         <label for="unidad" class="label">Presentacion: </label>
                         <div class="ccont col-12 ">
-                        <input class="entra" name="unidad" readonly value="<?php echo $consulta['unidad'] ?>">    
+                        <input class="entra" name="unidad" readonly value="<?php echo $consulta['unidad']; ?>">    
                         </div>
                         <label for="precio" class="label">Precio: </label>
                         <div class="ccont col-12">
-                            <input class="entra" name="precio" readonly value=" $ <?php echo $consulta['precio'] ?>">
+                            <input class="entra" name="precio" readonly value=" $ <?php echo $consulta['precio']; ?>">
                         </div>
                     </div>
                 </div>
@@ -64,8 +66,7 @@
                     </div>
 
                     <div class="contAcarro col-4 ">
-                        <button type="button" class="btnaddcar  rounded-5">Agregar <i class="fa-solid fa-cart-plus"
-                                style="margin-left: 8px; font-size: 1.3em;"></i></button>
+                        <button type="button" class="btnaddcar  rounded-5">Agregar <i class="fa-solid fa-cart-plus" style="margin-left: 8px; font-size: 1.3em;"></i></button>
                     </div>
                 </div>
         </div>
@@ -73,4 +74,13 @@
     </div>
 </div>
 
+<script>
+        let area = document.getElementById("entra2")
+        
+        window.addEventListener("DOMContentLoaded", () => {
+          area.forEach((elemento) => {
+            elemento.style.height = `${elemento.scrollHeight}px`
+          })
+        })    
+    </script>
 <!---fin ventana Update --->
